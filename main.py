dca8c536725a6465e0033b0cd80f5ad0b8dc7f33
Joshua Hecker <jhecker2001@gmail.com>
2023-11-06 08:35:01 -0600

    Added fields to enter website, username/email, and password, and added labels and nonfunctional buttons.

--- main.py
+++ main.py
@@ -4,13 +4,41 @@ from tkinter import *
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+# Window
 window = Tk()
 window.title("Password Manager")
 window.config(padx=20, pady=20)
 
+# Canvas
 logo = PhotoImage(file="logo.png")
 canvas = Canvas(width=200, height=200)
 canvas.create_image(100, 100, image=logo)
-canvas.pack()
+canvas.grid(column=1, row=0)
+
+# Website
+website_label = Label(text="Website:")
+website_label.grid(column=0, row=1)
+website_entry = Entry(width=35)
+website_entry.grid(column=1, row=1, columnspan=2)
+
+# Email/Username
+email_label = Label(text="Email/Username:")
+email_label.grid(column=0, row=2)
+email_entry = Entry(width=35)
+email_entry.grid(column=1, row=2, columnspan=2)
+
+# Password
+password_label = Label(text="Password:")
+password_label.grid(column=0, row=3)
+password_entry = Entry(width=17)
+password_entry.grid(column=1, row=3)
+
+# Generate Password
+generate_password_button = Button(text="Generate Password")
+generate_password_button.grid(column=2, row=3)
+
+# Add Entry
+add_button = Button(text="Add", width=30)
+add_button.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
