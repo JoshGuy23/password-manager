446fe91a53e0b7f3ee2a5fa3eda01ac86e933061
Joshua Hecker <jhecker2001@gmail.com>
2023-11-06 08:35:56 -0600

    Increased padding in window.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from tkinter import *
 # Window
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 
 # Canvas
 logo = PhotoImage(file="logo.png")
